3789258b95fc281bed5aec430fb93fb7769e6f3a
shivam saxena <shivamsaxenagr8@gmail.com>
2023-03-25 18:01:12 +0530

    fixed contact and added quoteRequestForm mailer service

--- quoteSender.php
+++ quoteSender.php
@@ -3,10 +3,11 @@
 if (isset($_POST['submitFromIndex'])){
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
-    $courseValue = $_POST['courseValue'];
+    $messageValue = $_POST['messageValue'];
+    
     $phoneNumber = $_POST['phoneNumber'];
     
-    $message = "Got quote request from website find the information below: \n \n $firstName $lastName \n course requested: $courseValue \n phone number $phoneNumber"
+    $message = "Got quote request from website find the information below: \n \n $firstName $lastName \n messageValue: $messageValue \n phone number $phoneNumber";
 
     $mailTo = "braindevelop.0408@gmail.com";
     
